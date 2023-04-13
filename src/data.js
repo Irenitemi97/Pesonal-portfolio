e10038f244d21fdab36963f7ca41668648184190
Irenitemi97 <adediransulyman@hotmail.com>
2023-04-13 18:05:28 +0100

    updated

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
 
 import img1 from './images/tab.png'
-import img2 from './images/smallridman.png'
-import img3 from './images/random.png'
+import img2 from './images/tsp.png'
+import img3 from './images/smallridman.png'
+import img4 from './images/random.png'
 export const cardlist =[
     {
         id: 1,
@@ -12,11 +13,17 @@ export const cardlist =[
     {
         id: 2,
         img: img2,
-        link: "https://www.smallridman.com/"
+        link: "https://irenitemi97.github.io/TSP-Portfolio/"
+       
     },
     {
         id: 3,
         img: img3,
+        link: "https://www.smallridman.com/"
+    },
+    {
+        id: 4,
+        img: img4,
         link: "https://randomadviser.netlify.app/"
     }
 ]
